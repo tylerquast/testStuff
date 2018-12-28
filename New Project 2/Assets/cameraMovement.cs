690d70ab5cba82ebb0c24af6ee9aeac840231fe1
Robert Taylor <Offthefront@gmail.com>
2018-12-27 21:59:52 -0800

    Drag and camera basic functionally
    God i hate descriptions

---
+++ New Project 2/Assets/cameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+    //public Transform MainCamera;
+    public Vector3 newPosition;
+    public float newScale;
+    private Camera newCam;
+	// Use this for initialization
+	void Start () {
+        
+        newPosition = transform.position;
+        newCam = Camera.main;
+        newScale = newCam.orthographicSize;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a"))
+        {
+            //left
+            newPosition.x = newPosition.x - (float)1;
+        }
+        if (Input.GetKey("d"))
+        {
+            //right
+            newPosition.x = newPosition.x + (float)1;
+
+        }
+        if (Input.GetKey("w"))
+        {
+            //up
+            newPosition.y = newPosition.y + (float)1;
+
+        }
+        if (Input.GetKey("s"))
+        {
+            //down
+            newPosition.y = newPosition.y - (float)1;
+
+        }
+        //scroll
+        newScale = newScale - 100 * Input.GetAxis("Mouse ScrollWheel");
+
+        transform.position = newPosition;
+        newCam.orthographicSize = newScale;
+    }
+}
---
+++ New Project 2/Assets/mouseDragable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseDragable : MonoBehaviour {
+    double offsetX;
+    double offsetY;
+	// Use this for initialization
+	public void dragStart() {
+        offsetX = Input.mousePosition.x;
+        offsetY = Input.mousePosition.y;
+    }
+	
+	// Update is called once per frame
+	public void drag() {
+       transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+	}
+}
